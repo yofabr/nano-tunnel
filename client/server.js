16a1bd1111dbbd2501dab3c36fc375f90c0b5a1c
YoftaheAbraham <jofab08@gmail.com>
2025-12-07 19:32:21 +0300

    websocket implementation

--- client/server.js
+++ client/server.js
@@ -1,7 +1,7 @@
-import { randomUUID } from "crypto";
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
+import { WebSocketServer } from "ws";
+import { randomUUID } from "crypto";
 
 const app = express();
 const server = http.createServer(app);
@@ -12,31 +12,36 @@ app.get("/api/hello", (req, res) => {
 });
 
 app.get("/send", (req, res) => {
-    io.emit("broad", "This is broadcaster!")
-    res.json({
-        message: "From send"
-    })
-})
-
-const io = new Server(server, {
-  cors: { origin: "*" }
+  // Broadcast message to all WebSocket clients
+  wss.clients.forEach((client) => {
+    if (client.readyState === 1) { // WebSocket.OPEN
+      client.send(JSON.stringify({ event: "broad", message: "This is broadcaster!" }));
+    }
+  });
+  res.json({ message: "From send" });
 });
+
+// Create WebSocket server
+const wss = new WebSocketServer({ server, path: "/ws" });
 
 let clients = {};
 
-io.path("/socket.io").on("connection", (socket) => {
-  console.log("New client connected:", socket.id);
+wss.on("connection", (ws) => {
+  const clientID = randomUUID();
+  clients[clientID] = ws;
+  console.log("New client connected:", clientID);
 
-  let clientID = randomUUID();
-  clients[socket.id] = clientID;
+  // Send welcome message
+  ws.send(JSON.stringify({ event: "welcome", clientID }));
 
-  socket.emit("welcome", {
-    clientID: clientID
+  // Receive messages from client
+  ws.on("message", (msg) => {
+    console.log("Received from client:", msg.toString());
   });
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnected:", socket.id);
-    delete clients[socket.id];
+  ws.on("close", () => {
+    console.log("Client disconnected:", clientID);
+    delete clients[clientID];
   });
 });
 
