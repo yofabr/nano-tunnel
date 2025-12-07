fe9f7d481f2e1b65576a95ede46c3f1be38065cb
YoftaheAbraham <jofab08@gmail.com>
2025-12-07 19:28:59 +0300

    socket-implementation

--- client/server.js
+++ client/server.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
 
 let clients = {};
 
-io.on("connection", (socket) => {
+io.path("/socket.io").on("connection", (socket) => {
   console.log("New client connected:", socket.id);
 
   let clientID = randomUUID();
